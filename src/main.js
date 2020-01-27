c133c7b49aca1d89f15ef3f00dc1d64d6bfcaa35
Kiswari10 <kiswari10@gmail.com>
2020-01-26 21:54:25 -0500

    Modifica main.js-funcion de guardar rutas de los archivos, test de la funcion agregada y creacion de carpeta de prueba para los tests

--- src/main.js
+++ src/main.js
@@ -20,9 +20,28 @@ const isMD = (route) => {
 
 const readDirectory = route => fs.readdirSync(route);
 
+const saveRoutesOfFiles = (route) => {
+  let arrayOfFiles = [];
+  const newRoute = typeOfPath(route);
+  if (isFile(newRoute) === true) {
+    if (isMD(newRoute) === true) {
+      arrayOfFiles.push(newRoute);
+    }
+  } else {
+    const data = readDirectory(newRoute);
+    data.forEach((elem) => {
+      const addRoute = path.join(newRoute, elem);
+      const file = saveRoutesOfFiles(addRoute);
+      arrayOfFiles = arrayOfFiles.concat(file);
+    });
+  }
+  return arrayOfFiles;
+};
+
 module.exports = {
   typeOfPath,
   isFile,
   isMD,
   readDirectory,
+  saveRoutesOfFiles,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const route = require('../src/main');
-
 
 describe('typeOfPath', () => {
   it('debería retornar una ruta absoluta', () => {
-    expect(route.typeOfPath('/home/kiswari/Desktop/firestore_prueba')).toBe('/home/kiswari/Desktop/firestore_prueba');
+    expect(route.typeOfPath(path.join(process.cwd(), 'src'))).toBe(path.join(process.cwd(), 'src'));
   });
   it('debería retornar una ruta absoluta al pasarle una ruta relativa', () => {
     expect(route.typeOfPath('./')).toBe(process.cwd());
@@ -12,24 +12,34 @@ describe('typeOfPath', () => {
 
 describe('isFile', () => {
   it('debería retornar true si la ruta es de un archivo', () => {
-    expect(route.isFile('/home/kiswari/Desktop/firestore_prueba/app.js')).toBe(true);
+    expect(route.isFile(path.join(process.cwd(), 'test', 'folder_example', 'app.js'))).toBe(true);
   });
   it('debería retornar false si la ruta es de un directorio', () => {
-    expect(route.isFile('/home/kiswari/Desktop/firestore_prueba')).toBe(false);
+    expect(route.isFile(path.join(process.cwd(), 'test', 'folder_example'))).toBe(false);
   });
 });
 
 describe('isMD', () => {
   it('debería retornar true si la ruta es de un archivo markdown (.md)', () => {
-    expect(route.isMD('/home/kiswari/Desktop/pruebasKis/README.md')).toBe(true);
+    expect(route.isMD(path.join(process.cwd(), 'README.md'))).toBe(true);
   });
   it('debería retornar false si la ruta es de un archivo con extension diferente a .md', () => {
-    expect(route.isMD('/home/kiswari/Desktop/firestore_prueba/app.js')).toBe(false);
+    expect(route.isMD(path.join(process.cwd(), 'package.json'))).toBe(false);
   });
 });
 
+const firstOutput = ['app.js', 'index.html', 'links.md', 'readme.md', 'style.css'];
+
 describe('readDirectory', () => {
   it('debería retornar un array con el contenido de la carpeta de la ruta', () => {
-    expect(route.readDirectory('/home/kiswari/Desktop/pruebasKis/src')).toEqual(['cli.js', 'main.js', 'md-links.js']);
+    expect(route.readDirectory(path.join(process.cwd(), 'test', 'folder_example'))).toEqual(firstOutput);
+  });
+});
+
+const secondOutput = [path.join(process.cwd(), 'test', 'folder_example', 'links.md'), path.join(process.cwd(), 'test', 'folder_example', 'readme.md')];
+
+describe('saveRoutesOfFiles', () => {
+  it('debería retornar un array con el contenido de la carpeta de la ruta', () => {
+    expect(route.saveRoutesOfFiles(path.join(process.cwd(), 'test', 'folder_example'))).toEqual(secondOutput);
   });
 });
